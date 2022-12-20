1be139ed2640fc8b1398752d0a1c8b97fdbf3d61
JesseUbuntu <jessenaidoo24@gmail.com>
2022-12-20 22:09:52 +0200

    Fixed backend

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -15,7 +15,7 @@ export const getUserFriends = async (req, res) => {
     const { id } = req.params;
     const user = await User.findById(id);
     const friends = await Promise.all(
-      user.friends.map((id2) => User.findById(id2))
+      user.friends.map((id) => User.findById(id))
     );
 
     const formattedFriends = friends.map(
@@ -45,31 +45,38 @@ export const getUserFriends = async (req, res) => {
   }
 };
 
-
 export const addRemoveFriend = async (req, res) => {
-    try {
-      const { id, friendId } = req.params;
-      const user = await User.findById(id);
-      const friend = await User.findById(friendId)
-      
-      if(user.friends.includes(friendId)){
-        user.friends = user.friends.filter((id) => id !== friendId);
-        friend.friends = friend.friends.filter((id2) => id2 !== id);
-      }else{
-        user.friends.push(friendId);
-        friend.friends.push(id);
-      }
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
 
-      await user.save();
-      await friend.save();
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
 
+    await user.save();
+    await friend.save();
 
-      const friends = await Promise.all(
-        user.friends.map((id2) => User.findById(id2))
-      );
-  
-      const formattedFriends = friends.map(
-        ({
+    const friends = await Promise.all(
+      user.friends.map((id2) => User.findById(id2))
+    );
+
+    const formattedFriends = friends.map(
+      ({
+        _id,
+        firstName,
+        lastName,
+        email,
+        occupation,
+        location,
+        picturePath,
+      }) => {
+        return {
           _id,
           firstName,
           lastName,
@@ -77,21 +84,10 @@ export const addRemoveFriend = async (req, res) => {
           occupation,
           location,
           picturePath,
-        }) => {
-          return {
-            _id,
-            firstName,
-            lastName,
-            email,
-            occupation,
-            location,
-            picturePath,
-          };
-        }
-      );
-
-
-    } catch (err) {
-      res.status(404).json({ message: err.message });
-    }
-  };
+        };
+      }
+    );
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
 import { verifyToken } from "./middleware/auth.js";
-import {createPost} from "./controllers/post.js"
+import {createPost} from "./controllers/posts.js"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
